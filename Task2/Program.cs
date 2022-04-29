1f662fa26f10e251948536805494487ba3204918
Alexander Novikov <a_noivkov@inbox.ru>
2022-04-29 10:21:48 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(num1, num2, num3);
+
