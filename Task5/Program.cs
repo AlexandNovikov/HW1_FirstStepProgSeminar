ceb8150219a41f231625a8f9c070d912a3eae93c
Alexander Novikov <a_noivkov@inbox.ru>
2022-05-15 22:09:01 +0300

    HomeWork2 Complete

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -5,4 +5,8 @@
 // 782 -> 8
 // 918 -> 1
 
+Console.Write("Введите трехзначное число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = (number / 10) % 10;
+Console.WriteLine(number1);
 
