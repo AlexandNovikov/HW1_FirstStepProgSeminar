4a3c0eb36facc4dcfb5c09eed5af25e10d4b133c
Alexander Novikov <a_noivkov@inbox.ru>
2022-05-28 23:08:25 +0300

    HW_Seminar_Complete

---
+++ Task36/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный
+// случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    Console.WriteLine("Мвссив: ");
+    Console.Write("[ ");
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        Console.Write( collection[index] + " ");
+        index++;
+    }
+    Console.Write("]");
+    Console.WriteLine();
+    
+ }
+
+void PrintArray(int[] print)   
+{
+    int count = print.Length;
+    int position = 0;
+    int sum = 0;
+Console.WriteLine("Нечетные элементы массива: ");
+    while (position < count)
+    {
+        if (print[position] % 2 != 0)
+            Console.Write(print[position] + " ");
+        if (print[position] % 2 != 0) sum += print[position];
+        position++;
+    }
+    Console.WriteLine();
+    Console.Write("Сумма нечетных элементов массива равна : " + sum);
+}
+Console.WriteLine("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N]; 
+
+FillArray(array);
+PrintArray(array);
--- Task38/Program.cs
+++ Task38/Program.cs
@@ -3,19 +3,19 @@
 // элементов массива.
 // [3 7 22 2 78] -> 76
 
-Console.Clear();
+
 Console.WriteLine();
 
 int size = new Random().Next(1, 11);
 
 Console.WriteLine($"Количетсов элементов массива: {size}");
 
-double[] FillArray(int size)
+double[] FillSourceArray(int size)
 {
     double[] array = new double[size];
     for (int i = 0; i < size; i++)
     {
-        array[i] = new Random().Next(); //* 100;
+        array[i] = new Random().NextDouble() * 100;
     }
     return array;
 }
@@ -26,45 +26,14 @@ void PrintDiffMinMax(double[] arr)
     {
         Console.WriteLine(arr[i]);
     }
-    double difference = 0;
-        difference = arr.Max() - arr.Min();
-    Console.WriteLine($"Минимальное значение массива {arr.Min()}, максимальное значение массива {arr.Max()}");
+    double difference = arr.Max() - arr.Min();
+    Console.WriteLine($"Минимальное значение массива {arr.Min()}"); 
+    Console.WriteLine($"Максимальное значение массива {arr.Max()}");
     Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
     Console.WriteLine();
 }
 
-PrintDiffMinMax(FillArray(size));
+PrintDiffMinMax(FillSourceArray(size));
 Console.WriteLine();
 
-// Console.Clear();
-// Console.WriteLine();
 
-// double size = new Random().Next(1, 11);
-
-// Console.WriteLine($"Количетсов элементов массива: {size}");
-
-// double[] FillArray(int size)
-// {
-//     double[] array = new double[size];
-//     for (int i = 0; i < size; i++)
-//     {
-//         array[i] = new Random().Next();
-//     }
-//     return array;
-// }
-
-// void PrintDiffMinMax(double[] arr)
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         Console.WriteLine(arr[i]);
-//     }
-//     double difference = 0;
-//     difference = arr.Max() - arr.Min();
-//     Console.WriteLine($"Минимальное значение массива {arr.Min()}, максимальное значение массива {arr.Max()}");
-//     Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
-//     Console.WriteLine();
-// }
-
-// PrintDiffMinMax(FillArray(size));
-// Console.WriteLine();
