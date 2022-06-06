94d2e40984e4f815b013d09a1c89f35cf50e918c
Alexander Novikov <a_noivkov@inbox.ru>
2022-06-06 19:08:54 +0300

    Task47, 50, 52 Complete

---
+++ Task47/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+System.Console.WriteLine();
+
+double[,] GetArrayDouble(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * 100;
+            
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write(arr[i, j] + "    ");
+            // Math.Round(arr, 2) не могу сократить!!!!
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int m = 3;
+int n = 4;
+double[,] ArrayDouble = GetArrayDouble(m, n);
+PrintArray(ArrayDouble);
